5430d2142016899e4647ec3212012a0aa73ccf5e
Ryan <ryank9462@gmail.com>
2022-02-21 12:07:37 -0600

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,24 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
 
+  let convertedTemp = (fahrenheit-32) * (5/9);
+
+  if (convertedTemp % 1 === 0) {
+    return convertedTemp;
+  } else {
+    return Number(convertedTemp.toFixed(1));
+  }
+  
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
 
+  let convertedTemp = celsius * (9/5) + 32;
+
+  if (convertedTemp % 1 === 0) {
+    return convertedTemp
+  } else {
+    return Number(convertedTemp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
