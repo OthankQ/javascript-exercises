478e971cd12176e030bbf791829d77aaa085b40f
Ryan <ryank9462@gmail.com>
2022-02-21 11:13:29 -0600

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    // Store the first argument in a new variable.
+    let originalArray = args[0];
+    let newArray = originalArray;
+    for (let i = 1; i < args.length; i++) {
+        newArray = newArray.filter((element) => {
+            return element !== args[i];
+        })
+    }
+    return newArray;
+    // for each of the arguments starting from the second one, filter 
+    // the element from the passed in first array argument and return
+    // the final result.
 };
 
 // Do not edit below this line
