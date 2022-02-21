1ac9e53b7b94387f2d14945607d02ef154152e25
Ryan <ryank9462@gmail.com>
2022-02-21 10:50:23 -0600

    Complete 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split('');
+    let newString = [];
+    for (let i = 0; i < string.length; i++){
+        newString.unshift(splitString[i])
+    }
+    return newString.join('');
+    // create an empty string
+    // iterate from the end of the given string and add each letter to the empty string.
+    // return the newly created string
 };
 
 // Do not edit below this line
